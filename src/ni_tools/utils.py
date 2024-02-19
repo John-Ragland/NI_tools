c35e32de84d562437f8600a9bb8b47f7495e8b29
John Ragland <jhrag@uw.edu>
2024-02-19 17:14:34 +0000

    added new SNR methods

--- src/ni_tools/utils.py
+++ src/ni_tools/utils.py
@@ -5,7 +5,8 @@ import numpy as np
 from scipy import signal
 import xarray as xr
 import scipy
-import xrsignal
+import xrsignal as xrs
+import xrft
 
 def freq_whiten(data, dim, b,a):
     '''
@@ -112,7 +113,89 @@ def SNR(
     snrs = {}
     # amplitude
     for peak in peaks:
-        amp = np.abs(xrsignal.hilbert(NCCFs.sel({dim:peaks[peak]}), dim=dim)).max(dim=dim)
+        amp = np.abs(xrs.hilbert(NCCFs.sel({dim:peaks[peak]}), dim=dim)).max(dim=dim)
         snrs[peak] = 20*np.log10(amp/noise_slice)
 
     return xr.Dataset(snrs)
+
+
+def SNRx(NCCFs : xr.DataArray, peaks : dict, noise : slice, dim : str='delay'):
+    '''
+    SNRx - compute SNR for dictionary of peaks.
+
+    $\mathrm{SNR} = 20 \log_{10} \left( \frac{\max(|x|)}{\sigma_n} \right)$
+    where $\sigma_n$ is the standard deviation of a slice with no peaks (specified by noise parameter)
+
+    **notes on application**
+    - this was created from code in OOI NI inversion/results.ipynb, and is the algorithm used for estimating
+        SNR in *Ragland et. al. Using ocean ambient sound to measure local integrated deep ocean temperature, 2024*
+    Parameters
+    ----------
+    NCCFs : xarray.DataArray
+        NCCFs to compute SNR for. NCCF should be cross-correlation (**NOT** the derivative of the cross-correlation).
+    peaks : dict
+        dictionary of peaks. keys are peak names and values are slices (in dim cooridinates)
+    noise : slice
+        slice to use for noise standard deviation
+    dim : str, optional
+        dimension to compute SNR along, by default 'delay'
+    '''
+    dNCCFs = NCCFs.differentiate(dim)
+
+    dNCCFc = xrs.hilbert(dNCCFs, dim=dim)
+
+    amps = {}
+    for peak in peaks:
+        amps[peak] = np.abs(dNCCFc.sel({dim:peaks[peak]})).max(dim=dim)
+
+    amps_x = xr.Dataset(amps)
+
+    noise = np.sqrt(((np.abs(dNCCFc)**2).sel({dim:noise})).mean(dim=dim))
+
+    snr = amps_x / noise
+
+    return snr
+
+def compute_fbarrms(NCCFs, peaks, dim='delay'):
+    '''
+    compute_fbarrms - compute the rms frequency bandwidth from NCCFs
+    '''
+
+    fbars = {}
+    for peak in peaks:
+        NCCFs_f = xrft.fft(NCCFs.sel({dim: peaks[peak]}), dim=dim)
+        Sxx_f = (NCCFs_f * np.conjugate(NCCFs_f)).real.sel({f'freq_{dim}':slice(0, None)})
+
+        fbar = (Sxx_f[f'freq_{dim}'] * (Sxx_f)).mean(f'freq_{dim}') / (Sxx_f).mean(f'freq_{dim}')
+        f2bar = (Sxx_f[f'freq_{dim}']**2 * (Sxx_f)).mean(f'freq_{dim}') / (Sxx_f).mean(f'freq_{dim}')
+
+        fbar_rms = np.sqrt(f2bar - fbar**2)
+
+        fbars[peak] = fbar_rms
+    
+    return xr.Dataset(fbars)
+
+
+def compute_sigma(NCCFs, peaks, dim='delay'):
+    '''
+    compute_sigma - compute the standard deviation of arrival time estimate
+
+    Parameters
+    ----------
+    NCCFs : xarray.DataArray
+        NCCFs to compute standard deviation for. NCCF should be cross-correlation (**NOT** the derivative of the cross-correlation).
+    peaks : dict
+        dictionary of peaks. keys are peak names and values are slices (in dim cooridinates)
+    dim : str, optional
+        delay dimension of noise cross-correlation functions (Default value = 'delay')
+    '''
+
+    dNCCFs = NCCFs.differentiate(dim)
+    snrs = SNRx(NCCFs, peaks, slice(-1,1), dim=dim)
+    fbars = compute_fbarrms(dNCCFs, peaks, dim=dim)
+
+    sigmas = {}
+    for peak in peaks:
+        sigmas[peak] = 1/(2*np.pi*fbars[peak] * snrs[peak])
+    
+    return xr.Dataset(sigmas)
